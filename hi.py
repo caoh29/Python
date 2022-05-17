208d01ba728d884c1cbf9a810d729aca3b40f9f9
caoh29 <cronox20@hotmail.com>
2022-05-17 09:06:04 -0500

    hello world

---
+++ hi.py
@@ -0,0 +1,8 @@
+#Hi.py
+#This programs says Hi
+
+print("Hi, I hope you are having a wonderful day")
+name = input("My name is Camilo and this is my Git repository, May I know your name?  ")
+print(f"Its a pleasure to meet you! {name.upper()}! \nI hope you find the information that you are looking for out here")
+print("If you have any doubts or concerns you can contact me at: cronox20@gmail.com")
+print("Thank you so much for taking the time to read this introduction, Have an amazing day!")
---
+++ hola.py
@@ -0,0 +1,8 @@
+#Hola.py
+#Este es un programa donde te saludaré
+
+print("Hola, Espero te encuentres muy bien el día de hoy")
+nombre = input("Mi nombre es Camilo y este es mi repositorio de Git, ¿Cuál es tu nombre?  ")
+print(f"Es un placer conocerte {nombre.upper()} \nEspero que halles la información que necesites aquí")
+print("Cualquier duda o inquietud me puedes contactar a: cronox20@gmail.com")
+print("¡Gracias y que tengas un excelente día!")
