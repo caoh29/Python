2957dc27f67f79ff651300b34f018641f5c8df88
caoh29 <cronox20@hotmail.com>
2022-10-07 08:09:15 -0500

    decorators first commit

---
+++ CS50W/decorators.py
@@ -0,0 +1,16 @@
+from curses import wrapper
+
+
+def announce(f):
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with function")
+    return wrapper
+
+
+@announce
+def hello():
+    print("Hello world!")
+
+hello()
