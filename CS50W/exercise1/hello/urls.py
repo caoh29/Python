3f8f85f3862c4ec647ff389b37c78718bd9824a7
caoh29 <cronox20@hotmail.com>
2022-10-11 10:03:11 -0500

    adding another function

--- CS50W/exercise1/hello/urls.py
+++ CS50W/exercise1/hello/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index")
+    path("camilo", views.camilo, name="camilo")
 ]
--- CS50W/exercise1/hello/views.py
+++ CS50W/exercise1/hello/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+def camilo(request):
+    return HttpResponse("Hello, Camilo")
