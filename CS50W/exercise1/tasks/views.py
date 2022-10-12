c239edd3b26a11177f351b02c638db55cb1ae1a9
caoh29 <cronox20@hotmail.com>
2022-10-12 09:18:29 -0500

    fixing bug on function

--- CS50W/exercise1/tasks/views.py
+++ CS50W/exercise1/tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 tasks = ["dormir", "estudiar", "trabajar", "reproducir", "morir"]
 # Create your views here.
-def tasksofyourlife(request, tasks):
+def tasksofyourlife(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
