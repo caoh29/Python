e77003504dece216d9281cc24183304e36777634
caoh29 <cronox20@hotmail.com>
2022-10-12 06:55:09 -0500

    fixing errors

--- CS50W/newyear/newyear/newyear/urls.py
+++ CS50W/newyear/newyear/newyear/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('newyear/', include("newyear.urls")),
+    path('nuevoano/', include("nuevoano.urls")),
 ]
