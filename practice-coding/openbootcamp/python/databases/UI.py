de93cd37e351d0232a9f2acca350f74205fa8ac2
Camilo Ordoñez <cronox20@hotmail.com>
2022-12-12 09:31:10 -0500

    parameters on the UI

--- practice-coding/openbootcamp/python/databases/UI.py
+++ practice-coding/openbootcamp/python/databases/UI.py
@@ -1,8 +1,24 @@
 import tkinter
 import sys
 
-window = tkinter.Tk()
+def main():
+    window = tkinter.Tk()
+
+    #Titulo de la ventana
+    titulo = 'Estudiantes'
+    window.title(titulo)
+
+    #Icono de la ventana
+    ruta = 'assets/books.png'
+    photo = tkinter.PhotoImage(file = ruta)
+    window.iconphoto(False, photo)
+
+    #Tamaño de la ventana
+    dimension = '500x500'
+    window.geometry(dimension)
 
 
+    window.mainloop()
 
-window.mainloop()
+if __name__ == '__main__':
+    main()
